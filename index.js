eea80e7b152519beebc225050b730e835db5b2f4
yesmukhamedov <yesmukhamedov.yeskendyr@gmail.com>
2023-10-22 17:04:35 +0600

    2222

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ import handleValidationErrors from "./utils/handleValidationErrors.js";
 import checkPermission from "./utils/checkPermission.js";
 import {config} from 'dotenv';
 
-// require('dotenv').config();777
-
 config();
 
 mongoose
@@ -51,18 +49,15 @@ app.post('/login', validation.login, handleValidationErrors, UserController.logi
 app.post('/register', validation.register, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-// app.get('/lessons', checkAuth, LessonController.getAll);
-// app.get('/lesson/:key', checkAuth, LessonController.getOne);
-// app.post('/lesson', checkAuth, checkPermission, validation.lessonForm, handleValidationErrors, LessonController.create);
-// app.delete('/lesson/:id', checkAuth, checkPermission, LessonController.remove);
-// app.patch('/lesson/:id', checkAuth, checkPermission, validation.lessonForm, handleValidationErrors, LessonController.update);
-
 app.get('/quiz/:collectionName/:count', checkAuth, TestController.getQuiz); //for student
 app.get('/collection/:collectionName', checkAuth, TestController.getCollection); //for teacher on admin panel
 
 app.post('/test', checkAuth, checkAuth, TestController.add);
 app.patch('/test/:id', checkAuth, TestController.update);
 app.delete('/test/:id', checkAuth, TestController.remove);
+
+app.get('/score', checkAuth, UserController.getMe);
+app.post('/checkMe', checkAuth, TestController.add);
 
 app.post('/upload', checkAuth, upload.single('recfile'), (req, res) => {
     res.json({status: {type: 'success', message: req.file.originalname, description: 'File сәтті сақталды'}, file: {url: `/uploads/${req.file.originalname}`}})
